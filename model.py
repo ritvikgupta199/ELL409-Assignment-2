51f14c3985e70e60f2e80d456af9e6417ea8da18
Ritvik Gupta <ritvikgupta199@gmail.com>
2021-10-22 15:47:40 +0530

    Added CVXOPT model

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import time
 import numpy as np
 import libsvm.svmutil as svm
+import cvxopt as co
 
 class LIBSVMModel:
     def __init__(self, c, kernel):
@@ -23,3 +24,42 @@ class LIBSVMModel:
         pred_labels, pred_acc, pred_val = svm.svm_predict(y_data, x_data, self.model, options = f'-q')
         pred_labels = np.array(pred_labels)
         return pred_labels
+
+class CVXModel:
+    def __init__(self, c, kernel):
+        self.c = c
+        self.kernel = kernel
+    
+    def get_linear_x(self, x1, x2):
+        return np.matmul(x1, x2.T)
+        
+    def get_gaussian_x(self, x1, x2):
+        return np.matmul(x1, x2.T)
+
+    def get_xmat(self, x1, x2):
+        if self.kernel == 'linear':
+            return self.get_linear_x(x1, x2)
+        else:
+            return self.get_gaussian_x(x1, x2)
+
+    def get_matrices(self, train_x, train_y):
+        n = train_y.shape[0]
+        P = np.outer(train_y, train_y) * self.get_xmat(train_x, train_x)
+        q = -1.0 * np.ones(train_y.shape)
+        G = np.concatenate((-1.0*np.eye(n), np.eye(n)))
+        h = np.concatenate((np.zeros(train_y.shape), self.c*np.ones(train_y.shape)))
+        A = train_y.reshape(1,n)
+        b = 0.0
+        P = co.matrix(P)
+        q = co.matrix(q)
+        G = co.matrix(G)
+        h = co.matrix(h)
+        A = co.matrix(A)
+        b = co.matrix(b)
+        return P, q, G, h, A, b
+
+    def train(self, train_x, train_y):
+        P, q, G, h, A, b = self.get_matrices(train_x, train_y)
+        sol = co.solvers.qp(P, q, G, h, A, b, options={'show_progress': True})
+        alphas = np.array(sol['x'])
+        return alphas
--- trainer.py
+++ trainer.py
@@ -1,9 +1,14 @@
 import numpy as np
 from dataloader import DataLoader, TestDataLoader
-from model import LIBSVMModel
+from model import LIBSVMModel, CVXModel
 
 def train_binary(data_path, c, split, cls):
     dataloader = DataLoader(data_path)
     train_x, train_y, valid_x, valid_y = dataloader.get_binary_data(split, cls)
-    model = LIBSVMModel(c, 'test')
-    model.train(train_x, train_y)
+    # model = LIBSVMModel(c, 'test')
+    # model.train(train_x, train_y)
+    model = CVXModel(10, 'linear')
+    alphas = model.train(train_x, train_y)
+    print(alphas)
+
+train_binary('data/2019MT10512.csv',10, 0.5, (1,2))
